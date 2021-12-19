c5619b0fc098c823c00f3256290947c12003dc8b
hecuhecu <hecunyuji5@gmail.com>
2021-12-19 15:06:00 +0900

    Initial Commit

---
+++ hirokiKadai3/hirokiKadai3App.swift
@@ -0,0 +1,17 @@
+//
+//  hirokiKadai3App.swift
+//  hirokiKadai3
+//
+//  Created by 河村宇記 on 2021/12/19.
+//
+
+import SwiftUI
+
+@main
+struct hirokiKadai3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
